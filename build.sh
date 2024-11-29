4f74696a27f0392df83ae6431a73b5633529f420
ali <muhammadalihassanofficial@gmail.com>
2024-11-29 10:22:33 +0500

    feat: add build file Build.sh compiles and executes the file which is given as first argument after the command e.g './build.sh binarySearch'

---
+++ build.sh
@@ -0,0 +1,28 @@
+SOURCE_FILE="$1.cpp"
+
+
+SOURCE_FILE_PATH=$(find . -type f -name "$SOURCE_FILE")
+
+if [ -z "$SOURCE_FILE_PATH" ] ; then
+  echo "Error: File $SOURCE_FILE not found"
+  exit 1
+fi
+
+mkdir -p build
+# Location of build file
+EXECUTABLE_FILE=./build/main.exe
+# compile the source code
+clang++ -std=c++23 -ggdb -pedantic-errors -Wall -Weffc++ -Wextra  $SOURCE_FILE_PATH -o $EXECUTABLE_FILE  && $EXECUTABLE_FILE
+
+
+if [ $? -eq 0 ]; then 
+  echo "All tests passed successfully.";
+else 
+  echo "Some tests are failing!!. Check your program logic.";
+  exit 1;
+fi
+
+
+# Removed flags
+# -Werror
+# -Wconversion -Wsign-conversion 
