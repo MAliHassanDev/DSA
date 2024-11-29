c9fc26776e951434516cda8689c09288ef0e32b7
ali <muhammadalihassanofficial@gmail.com>
2024-11-29 10:24:55 +0500

    feat: add binary search algorithm

---
+++ src/BinarySearch/binarySearch.cpp
@@ -0,0 +1,34 @@
+#include <cmath>
+#include <iostream>
+#include <vector>
+#include <cassert>
+
+class Solution {
+ public:
+  int binary_search(std::vector<int>& nums, int target) {
+    int left = 0;
+    int right = nums.size() - 1;
+    int result = -1;
+    while (left <= right) {
+      int mid = floor(left + (right - left) / 2);
+      if (nums[mid] == target) {
+        result = mid;
+        break;
+      } else if (nums[mid] < target) {
+        left = mid + 1;
+      } else {
+        right = mid - 1;
+      }
+    }
+    return result;
+  }
+};
+
+
+int main() {
+  Solution sol;
+  std::vector<int> nums = {1, 2, 3, 4, 5};
+  int target = 4;
+  int result = sol.binary_search(nums, target);
+  assert("Result should be 4" && result == 3);
+}
